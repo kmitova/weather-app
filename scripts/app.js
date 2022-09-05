ad9424266e2924b14334531ae5aeb818730cd779
kmitova <96522051+kmitova@users.noreply.github.com>
2022-09-05 15:23:58 +0300

    work on forecast api

--- scripts/app.js
+++ scripts/app.js
@@ -1,28 +1,49 @@
 // "https://api.openweathermap.org/data/2.5/weather?q=London&APPID=bcf2d6ffcb93077ae5816ec9f9ba72b8&units=metric";
 import { fetchWeather } from "./api.js";
-// import { displayWeather } from "./display.js";
-// console.log("hello");
+import { fetchForecast } from "./forecastApi.js";
 
 const location = document.getElementById("location");
 const searchBtn = document.getElementById("search-btn");
 
 searchBtn.addEventListener("click", () => {
   fetchWeather();
+  fetchForecast();
 });
 
-// async function fetchWeather() {
+// async function fetchForecast() {
 //   try {
-//     const response = await fetch(
-//       "https://api.openweathermap.org/data/2.5/weather?q=London&APPID=bcf2d6ffcb93077ae5816ec9f9ba72b8&units=metric"
-//     );
+//     const api = "https://api.openweathermap.org/data/2.5/forecast?q=";
+//     const city = document.getElementById("location").value;
+//     const apiKey = "&APPID=bcf2d6ffcb93077ae5816ec9f9ba72b8";
+//     const units = "&units=metric";
+//     const url = api + city + apiKey + units;
+
+//     const response = await fetch(url);
 //     const data = await response.json();
-//     console.log(data);
-//     console.log(data.main.feels_like);
-//     console.log(data.name);
-//     console.log(data.wind.speed);
-//     console.log(data.main.temp);
-//     console.log(data.weather[0].main);
+//     console.log(data.list[0].main.temp);
+//     console.log(data.list[0].weather[0].main);
+
+//     const forecast = getForecast(data);
 //   } catch (err) {
-//     console.log("Something went wrong...");
+//     console.log(err);
 //   }
+// }
+
+// function getForecast(data) {
+//   const forecast = {};
+
+//   let count = 0;
+
+//   for (let index = 0; index < data.list.length; index += 8) {
+//     forecast[count] = [
+//       data.list[index].main.temp,
+//       data.list[index].weather[0].main,
+//     ];
+//     console.log(data.list[index]);
+//     count++;
+//   }
+
+//   console.log(forecast);
+
+//   return forecast;
 // }
